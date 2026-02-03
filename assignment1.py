5a83125a5ff3308a0cb1288244851dd88d3a418c
Sadie Lamplot <slamplot@unomaha.edu>
2026-02-02 19:58:00 -0600

    PyGAM version
    Replaced ExponentialSmoothing model with LinearGAM for forecasting trips based on hour, day of week, and trend features.

--- assignment1.py
+++ assignment1.py
@@ -1,18 +1,31 @@
 import pandas as pd
-from statsmodels.tsa.holtwinters import ExponentialSmoothing
+import numpy as np
+from pygam import LinearGAM, s, f
 
 train = pd.read_csv("assignment_data_train.csv")
 
-# Target variable
-y = train["trips"]
+# Target
+y = train["trips"].values
 
-model = ExponentialSmoothing(
-    y,
-    trend="add",
-    seasonal="add",
-    seasonal_periods=24
-)
+# Features: hour of day, day of week, trend
+X = pd.DataFrame({
+    "hour": train.index % 24,                     # hourly seasonality
+    "day_of_week": (train.index // 24) % 7,      # weekly seasonality
+    "trend": np.arange(len(train))               # overall trend
+})
 
-modelFit = model.fit()
+model = LinearGAM(
+    s(0, n_splines=24, spline_order=3) +   # hour
+    f(1) +                                 # day of week as factor
+    s(2, n_splines=50)                     # trend
+).fit(X.values, y)
 
-pred = modelFit.forecast(744)
+modelFit = model  # autograder expects modelFit
+
+X_pred = pd.DataFrame({
+    "hour": np.arange(len(train), len(train)+744) % 24,
+    "day_of_week": (np.arange(len(train), len(train)+744)//24) % 7,
+    "trend": np.arange(len(train), len(train)+744)
+})
+
+pred = modelFit.predict(X_pred.values)
